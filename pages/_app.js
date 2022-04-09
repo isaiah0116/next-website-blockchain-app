cd40ef1745b1dfa5fd4c0605cc759b9470e3db0c
isaiah0116 <isaiah0116@gmail.com>
2022-04-09 19:39:58 -0400

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId={"HPqe3okZh1D0Yrzi7tydb8Yqq6T0WFJaXipvu0iA"} //process.env.NEXT_PUBLIC_APP_ID
+      serverUrl={"https://x3ndg09zcrzz.usemoralis.com:2053/server"} //process.env.NEXT_PUBLIC_SERVER_URL
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );  
 }
 
 export default MyApp
